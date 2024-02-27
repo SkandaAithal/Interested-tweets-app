7bb8d9de46cb812f39b0abb4e70758c5d4354866
gagan_971 <vgagan971@gmail.com>
2024-02-27 16:30:52 +0530

    commit 2

--- Interested-Tweets-Server/src/main.ts
+++ Interested-Tweets-Server/src/main.ts
@@ -1,20 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import * as dotenv from "dotenv";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
-    .setTitle('Tweets-Back-End API')
-    .setDescription('The Tweets API description')
-    .addTag('Tweets')
+    .setTitle("Tweets-Back-End API")
+    .setDescription("The Tweets API description")
+    .addTag("Tweets")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
