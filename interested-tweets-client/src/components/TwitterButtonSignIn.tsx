db52cf81bd60e376e31a22050daa1d2d4e4cd357
gagan_971 <vgagan971@gmail.com>
2024-02-29 16:54:49 +0530

    Twitter auth v2

--- interested-tweets-client/src/components/TwitterButtonSignIn.tsx
+++ interested-tweets-client/src/components/TwitterButtonSignIn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaTwitter } from "react-icons/fa";
 
 export default function TwitterButtonSignIn() {
-  return (<a href="https://localhost:3001/auth/login">
+  return (<a href="http://localhost:3001/auth/login">
     <button className="flex items-center w-full justify-center gap-2 px-4 py-2 bg-blue-400 text-white rounded-md shadow-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-200">
       <FaTwitter className="text-white" />
       Sign In with Twitter
