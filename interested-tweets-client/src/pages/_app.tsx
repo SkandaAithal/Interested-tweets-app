67e1311077f5dbf1b69485daab8a0c58d790401e
Skanda Aithal K R <skandaaithal1@gmail.com>
2024-03-07 12:24:20 +0530

    Added analytics

--- interested-tweets-client/src/pages/_app.tsx
+++ interested-tweets-client/src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import Layout from "@/components/Layout";
 import { GlobalContextProvider } from "@/context/globalState";
 import "@/styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GlobalContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GlobalContextProvider>
+    <>
+      <GlobalContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GlobalContextProvider>
+      <SpeedInsights />
+    </>
   );
 }
